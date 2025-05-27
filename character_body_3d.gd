154fa6c45dbcb5fbac3d174bb13cc449457aaa15
Shibe-hash <neonnecro97@gmail.com>
2025-05-26 22:12:36 -0400

    jump, camera movement, lighting added

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,17 +1,45 @@
 extends CharacterBody3D
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var speed = 4.0  # movement speed
-var jump_speed = 6.0  # determines jump height
-var mouse_sensitivity = 0.002  # turning speed
+# Movement parameters
+@export var speed = 5.0
+@export var jump_velocity = 4.5
+@export var mouse_sensitivity = 0.002
+@export var gravity = 9.8
 
+# Camera controls
+@onready var camera_pivot = $CameraPivot
+@onready var camera = $CameraPivot/Camera3D
 
-func get_input():
-	var input = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
-	velocity.x = input.x * speed
-	velocity.z = input.y * speed
+func _ready():
+	# Hide and capture mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	# Mouse look for camera rotation
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		camera_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -1.5, 1.5)
 
 func _physics_process(delta):
-	velocity.y += -gravity * delta
-	get_input()
+	# Handle movement
+	var input_dir = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	# Ground movement
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
 	move_and_slide()
